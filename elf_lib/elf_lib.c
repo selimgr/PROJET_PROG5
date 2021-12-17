5a1e573b2bd81ffe1eb4b199f156ba6c3bcd21b9
selimgr <jaawis.dev@gmail.com>
2021-12-18 00:19:48 +0100

    elf lib base

--- elf_lib/elf_lib.c
+++ elf_lib/elf_lib.c
@@ -1,24 +1,37 @@
 #include "elf_lib/elf_lib.h"
 
+/* Étape 1 */
 void init_header(FILE *f, void * elf_h) {
-    printf("init\n");
-    
-    elf_h = ((Elf32 *) elf_h);
 
-    fread(&((Elf32 *) elf_h)->e_ident, 16, 1, f);
-    fread(&((Elf32 *) elf_h)->e_type, 2, 1, f);
-    fread(&((Elf32 *) elf_h)->e_machine, 2, 1, f);
 }
-void write_elf (FILE *f, void * elf_h);
 
-/* Etape 2 */
-/* readelf -S */
-void read_sections(FILE *f, void * elf_h, void ** arr_elf_SH);
-void read_section(FILE *f, void * elf_h, void * elf_SH);
-void print_sections_header(FILE *f, void ** arr_elf_SH);
-void print_section_header(FILE *f, void * elf_SH);
+void write_elf (FILE *f, void * elf_h) {
 
-/* Etape 3 */
+} 
+
+/* Étape 2 */
+void read_sections(FILE *f, void * elf_h, void ** arr_elf_SH) {
+
+}  
+
+void read_section(FILE *f, void * elf_h, void * elf_SH) {
+
+}
+
+void print_sections_header(FILE *f, void ** arr_elf_SH) {
+
+}
+
+void print_section_header(FILE *f, void * elf_SH) {
+
+}
+
+/* Étape 3 */
 /* readelf -x num */
-void read_data_section(FILE *f, void * elf_SH);
-void print_data_section(FILE *f, void * elf_SH);
+void read_data_section(FILE *f, void * elf_SH) {
+
+}
+
+void print_data_section(FILE *f, void * elf_SH) {
+
+}
